bba3f053dcd953c48e9cdb61c5281cd6016a9d7d
Tessa <Tessachi33@yahoo.com>
2015-10-29 10:13:14 -0700

    Add al getter and setters for Choice model

--- app/src/main/java/com/epicodus/storytime/model/Choice.java
+++ app/src/main/java/com/epicodus/storytime/model/Choice.java
@@ -5,6 +5,22 @@ package com.epicodus.storytime.model;
  */
 public class Choice {
 
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
     private String mText;
     private int mNextPage;
 
